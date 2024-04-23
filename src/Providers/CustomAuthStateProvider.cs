3f7d5b47d1e34b6760ebdfff934b1ab83e22df34
Samra Mammadli <memmedlisemra6@gmail.com>
2024-04-24 00:04:11 +0400

    Add Home and Login Page

---
+++ src/Providers/CustomAuthStateProvider.cs
@@ -0,0 +1,90 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Json;
+using System.Security.Claims;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.IdentityModel.Tokens;
+
+namespace src.Providers;
+
+
+public class CustomAuthStateProvider : AuthenticationStateProvider
+{
+    private readonly JwtSecurityTokenHandler jwtTokenHandler;
+    private readonly ILocalStorageService localStorageService;
+
+    public CustomAuthStateProvider(
+        ILocalStorageService localStorageService)
+    {
+        this.localStorageService = localStorageService;
+        this.jwtTokenHandler = new JwtSecurityTokenHandler();
+    }
+
+    private async Task<ClaimsIdentity> GetClaimsIdentityAsync(string? jwt)
+    {
+        if (string.IsNullOrWhiteSpace(jwt))
+        {
+            return new ClaimsIdentity();
+        }
+
+        var validationResult = await jwtTokenHandler.ValidateTokenAsync(
+            jwt,
+            new TokenValidationParameters()
+            {
+                ValidateAudience = true,
+                ValidAudience = "4FITBODY",
+
+                ValidateIssuer = true,
+                ValidIssuer = "identity.akshambazari.az",
+
+                SignatureValidator = (token, validationParameters) => new JwtSecurityToken(token),
+
+                ValidateLifetime = true,
+                RequireExpirationTime = true,
+                LifetimeValidator = (notBefore, expires, securityToken, validationParameters) => expires > DateTime.UtcNow,
+            }
+        );
+
+        if (validationResult.IsValid == false)
+        {
+            if (validationResult.Exception is SecurityTokenInvalidLifetimeException lifetimeException)
+            {
+                var httpClient = new HttpClient();
+
+                var updateTokenResponse = await httpClient.PutAsJsonAsync("", new { jwt });
+
+                if (updateTokenResponse.IsSuccessStatusCode && updateTokenResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var newJwt = await updateTokenResponse.Content.ReadAsStringAsync();
+
+                    await this.localStorageService.SetItemAsStringAsync("jwt", newJwt);
+
+                    var newToken = jwtTokenHandler.ReadJwtToken(newJwt);
+
+                    return new ClaimsIdentity(newToken.Claims, "jwt");
+                }
+            }
+
+            return new ClaimsIdentity();
+        }
+
+        var token = jwtTokenHandler.ReadJwtToken(jwt);
+
+        return new ClaimsIdentity(token.Claims, "jwt");
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var jwt = await this.localStorageService.GetItemAsStringAsync("jwt");
+
+        var claimsIdentity = await this.GetClaimsIdentityAsync(jwt);
+
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+        var authenticationState = new AuthenticationState(claimsPrincipal);
+
+        base.NotifyAuthenticationStateChanged(Task.FromResult(authenticationState));
+
+        return authenticationState;
+    }
+}
