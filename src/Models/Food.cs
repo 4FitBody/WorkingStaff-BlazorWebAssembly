09bf3f7944b70fa2227a0c98bafb33d61504a431
Samra Mammadli <memmedlisemra6@gmail.com>
2024-04-25 19:36:37 +0400

    Add Food&Exercise

---
+++ src/Models/Food.cs
@@ -0,0 +1,18 @@
+namespace src.Models;
+
+public class Food
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? VideoUrl { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public string? Diet { get; set; }
+
+    public bool IsApproved { get; set; }
+}
---
+++ src/Models/SportSupplement.cs
@@ -0,0 +1,20 @@
+namespace src.Models;
+
+public class SportSupplement
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? ManufactureCountry { get; set; }
+
+    public double? Quantity { get; set; }
+
+     public bool IsApproved {get; set;} 
+
+
+}
